f99c0e96a02e27157bc24e6f0ed2cc84048a4d3e
hua zhou <huazhou2@gmail.com>
2019-03-15 17:39:30 -0400

    bootstrap css

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+import Popper from 'popper.js';
 import {Route, Switch, BrowserRouter as Router, Link} from 'react-router-dom';
 import Main from './main.jsx';
 import Product from './product.jsx';
@@ -33,67 +35,102 @@ class Header extends Component {
 
   topright_menu() {
     const {loggedin, name} = this.props;
-    return loggedin ? (
-      <ul>
-        <li className="dropdown">
-          {name}
-          <div className="dropdown-content">
-            <span>
-              <Link to="/accounts/:id">Your account</Link>
-            </span>
-            <span>
-              <Link to="/logout">Logout</Link>
-            </span>
-          </div>{' '}
-        </li>
-      </ul>
+    return (
+	     <ul className="navbar-nav ml-auto mt-2 mt-lg-3 ">
+        <li className="nav-item dropdown">
+              { loggedin ? (
+          <a
+            href="#"
+	    className="dropdown-toggle"
+            data-toggle="dropdown"
+	    role="button"
+	    aria-haspopup="true"
+            aria-expanded="false">
+            {name}
+          </a>):(
+          <a
+            href="#"
+            data-toggle="dropdown"
+	    role="button"
+	    aria-haspopup="true"
+            aria-expanded="false">
+            {name}
+          </a>)}
+
+              { loggedin ? (
+        <ul className="dropdown-menu">
+          <li>
+            <Link to="/accounts/:id">Your account</Link>
+          </li>
+          <li>
+            <Link to="/logout">Logout</Link>
+          </li>
+  </ul>
     ) : (
       <Link to="/login">Signin/Register</Link>
-    );
+
+    )}</li></ul> )
+
+
   }
   render() {
     const {data} = this.props;
     return (
       <Router>
         <div>
-          <div className="jumbotron text-center">
-            <h1>Lily Massage Supplies</h1>
-            <a href="/">
-              <img
-                src="/static/headimg.jpeg"
-                className="rounded center-block"
-                style={{width: 'auto', maxHeight: '100px'}}
-                alt="not here"
-              />
-            </a>
-          </div>
-          <div className="row">
-            <div className="col-sm-2" />
-            <div className="col-sm-8">
-              <div className="center_head">
-                <ul>
-                  {this.getGroup1().map((item, i) => (
-                    <li className="dropdown" key={i}>
-                      {item}
-                      <div className="dropdown-content">
-                        {this.getGroup2(item).map((item2, j) => (
-                          <span key={j}>
-                            <Link to={`/groups/${item2}`}>{item2}</Link>
-                          </span>
-                        ))}
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#storemenu">
+              <span className="navbar-toggler-icon" />
+            </button>
 
-            <div className="col-sm-2">
-              <span className="sign_in" key={11}>
-                {this.topright_menu()}
-              </span>
+            <div className="collapse navbar-collapse show" id="storemenu">
+              <a className="navbar-brand" href="/">
+                Lily Massage Supplies{' '}
+              </a>
+              <ul className="navbar-nav ml-auto mt-2 mt-lg-3">
+                {this.getGroup1().map((item, i) => (
+                  <li className="nav-item dropdown" key={i}>
+                    <a
+                      href="#"
+                      className="dropdown-toggle"
+                      data-toggle="dropdown"
+                      role="button"
+                      aria-haspopup="true"
+                      aria-expanded="false">
+                      {item}
+                    </a>
+                    <ul className="dropdown-menu">
+                      {this.getGroup2(item).map((item2, j) => (
+                        <li key={j}>
+                          <Link to={`/groups/${item2}`}>{item2}</Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                ))}
+              </ul>
+	     
+	       {this.topright_menu()}
+
+              <form className="form-inline mx-auto my-2 my-lg-3">
+                <input
+                  className="form-control mr-sm-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button
+                  className="btn btn-outline-success my-2 my-sm-0"
+                  type="submit">
+                  Search
+                </button>
+              </form>
             </div>
-          </div>
+          </nav>
           <Switch>
             <Route exact path="/" render={() => <Main data={data} />} />
             <Route
